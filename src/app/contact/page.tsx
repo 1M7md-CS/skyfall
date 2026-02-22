00c28b63d58401c91810213d6d4be891a4a1f415
1M7md-CS <mohammadhammad464@gmail.com>
2026-02-22 15:57:10 +0300

    add contact page structure

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "@/components/ui/Hero";
+import { Contact, Info } from "../../components/sections/contact";
+
+export default function contactPage() {
+	return (
+		<div className="min-h-screen">
+			<Hero
+				header="Contact Us"
+				description="Get in touch with us today and let us help you with any questions or inquiries you may have."
+			/>
+			<Contact />
+
+			<Hero header="Frequently Asked Questions" />
+			<Info />
+		</div>
+	);
+}
---
+++ src/components/sections/contact/Contact.tsx
@@ -0,0 +1,5 @@
+export default function Contact() {
+	return (
+		<div className="border-foreground border text-center">Contact Form</div>
+	);
+}
